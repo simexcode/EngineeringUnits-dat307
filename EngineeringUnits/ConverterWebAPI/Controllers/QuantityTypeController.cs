d3a38929262d7bf33426c35cac2fd6f3809154a0
Rune Alexander <alexander.laursen@gmail.com>
2020-12-15 20:24:08 +0100

    new controllers

--- EngineeringUnits/ConverterWebAPI/Controllers/QuantityTypeController.cs
+++ EngineeringUnits/ConverterWebAPI/Controllers/QuantityTypeController.cs
@@ -5,22 +5,22 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using UnitConverter;
+using System.Text.Json;
 
 namespace ConverterWebAPI.Controllers {
 
     [ApiController]
     [Route("[controller]")]
-    public class ConverterController : ControllerBase {
+    public class QuantityTypeController : ControllerBase {
         private readonly Converter converter;
 
-        public ConverterController() {
+        public QuantityTypeController() {
             this.converter = new Converter();
         }
 
         public class ConversionResult {
             public double data { get; set; }
             public string unit { get; set; }
-            public string name { get; set; }
         }
 
         public class ConerterInput {
@@ -29,11 +29,26 @@ namespace ConverterWebAPI.Controllers {
             [Required] public string to { get; set; }
         }
 
+        [HttpGet]
+        public string Start() {
+            List<string> list = new List<string>();
+            var File = converter.GetQuantityTypes();
+
+            for (int i = 0; i < File.Count; i++)
+            {
+                // for (int j = 0; j < File[i].Name.Length; j++)
+                // {
+                    // list.Add(File[i].descriptors[j]);
+                    System.Console.WriteLine(File[i].Name);
+                // }
+            }
+            return JsonSerializer.Serialize(list);
+        }
+
         [HttpPost]
         public ConversionResult Convert(ConerterInput input) {
-            Unit endUnit;
-            var res = converter.Convert(input.data, input.from, input.to, out endUnit);
-            return new ConversionResult { data = res.Item1, unit = res.Item2, name = endUnit.Name };
+            var res = converter.Convert(input.data, input.from, input.to);
+            return new ConversionResult { data = res.Item1, unit = res.Item2 };
         }
     }
 }
