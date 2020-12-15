1ec7d099820ea6f164e58182048aba145d38c1fa
Mathias Langkås <mathil14@uia.no>
2020-12-15 20:19:50 +0100

    updated dll

--- EngineeringUnits/UnitConverterConsoleTest/Program.cs
+++ EngineeringUnits/UnitConverterConsoleTest/Program.cs
@@ -1,9 +1,35 @@
 ﻿using System;
+using UnitConverter;
 
 namespace UnitConverterConsoleTest {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+            //var units = converter.GetUnitsInDimension("Length");
+            /*
+             var units = converter.GetUnitsInQuantity("temperature per time");
+             foreach (var unit in units) {
+                 Console.WriteLine(unit.Name);
+             }
+            */
+            Console.ReadKey();
+            Converter converter = new Converter();
+            try {
+                var result = converter.Convert(983, "cm", "m");
+                Console.WriteLine("GOT: " + result.Item1 + " " + result.Item2);
+                result = converter.Convert(983, "degC", "K");
+                Console.WriteLine("GOT: " + result.Item1 + " " + result.Item2);
+                result = converter.Convert(983, "kg", "t");
+                Console.WriteLine("GOT: " + result.Item1 + " " + result.Item2);
+                result = converter.Convert(983, "cm", "m");
+                Console.WriteLine("GOT: " + result.Item1 + " " + result.Item2);
+
+            }
+            catch (Exception e) {
+                Console.WriteLine("ERROR: " + e.Message);
+            }
+
+            Console.ReadKey();
         }
     }
 }
